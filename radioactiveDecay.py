ab1f62c15b2a9d313597ad158bd88e206140b1ba
Alex Wheaton <34541330+aswheaton@users.noreply.github.com>
2018-02-11 18:50:59 +0000

    Reformatted some strings. Added some dialogue.

--- radioactiveDecay.py
+++ radioactiveDecay.py
@@ -16,14 +16,16 @@ def main():
     sample = Sample(size, decayConstant)
 
     elapsedTime = 0.0
-
+    
+    print('Beginning simulation...')
+    
     while sample.getDecayedNuclei() < sample.getUndecayedNuclei():
 
         sample.stepForward(timeInterval)
         elapsedTime = elapsedTime + timeInterval
 
     sample.show() 
-    print('Simulation complete. There remain ' + str(sample.getDecayedNuclei()) + 'decayed nuclei and ' + str(sample.getUndecayedNuclei()) + ' undecayed nuclei.')
+    print('Simulation complete. There remain ' + str(sample.getDecayedNuclei()) + ' decayed nuclei and ' + str(sample.getUndecayedNuclei()) + ' undecayed nuclei.')
     print('The total elapsed time is ' + str(elapsedTime) + ' minutes.')
 
 main()
