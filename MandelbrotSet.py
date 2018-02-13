6bee182655122af1421702c730010f15658665e2
Alex Wheaton <34541330+aswheaton@users.noreply.github.com>
2018-02-13 11:10:34 +0000

    Added files via upload.

---
+++ MandelbrotSet.py
@@ -0,0 +1,13 @@
+"""
+    A class holding a Mandelbrot Set associated with a particular complex number.
+    Author: Alexander S. Wheaton
+    Date: 12th February 2018
+    Updated: 12th February 2018
+"""
+
+import math
+
+def MandelbrotSet(object):
+
+    def __init__(initialParameter, complexParameter, iterations):
+        
