8664bcf9736d78e9b044d4b2d59490ac91767839
Alex Wheaton <34541330+aswheaton@users.noreply.github.com>
2018-02-14 13:29:29 +0000

    Overhauled entire script. Eliminated class code.

--- plotFractal.py
+++ plotFractal.py
@@ -1,72 +1,27 @@
-
 """
-    A script that plots a fractal for a given function.py file.
+    A script that plots a fractal image of iterations to a given threshold for a
+    particular initial parameter (normally 0) and over a given range of complex
+    parameters.
+
     Author: Alexander S. Wheaton
     Date: 12th February 2018
-    Updated: 13th February 2018
+    Updated: 14th February 2018
 """
 
 import cmath
-import matplotlib
 
-def class MathematicalSet(object): 
-    
-    def __init__(self, initialParameter, complexParameter, thresholdValue, maxIterations):
-    
-        self.set = []
-        z = initialParameter
-        
-        for iterations in range(0, maxIterations):
-            self.set.append(z**2 + complexParameter)
-            
-        for iterations in range(0, maxIterations): 
-            if abs(self.set[iterations])**2 > thresholdValue:
-                self.iterationsToThreshold = iterations
-                break
-    
-    def getIterationsToThreshold():
-        return iterationsToThreshold
-
-# Initialises a array like list of lists containing mathematical sets.
-# This will be a class, later on, that recieves a particular function.
-
-def class FractalSet(object):
-    
-    def __init__(self, minimum, maximum, step, initialParameter, thresholdValue, maxIterations):
-        
-        self.set = []
-        
-        for realComponent in range(minimum.real, maximum.real, step):
-            self.set.append([])
-            for imaginaryComponent in range(minimum.imag, maximum.imag, step):
-                self.set.append()
-
-# Initialises an array-like list of lists containting the iterations to a threshold value for a particular complex parameter.
-
-def class FractalImage(object):
-
-    def __init__(self):
-    
-    self.fractalImage = []
-    
-    for row in range(imageCenter.real-imageRowSize/2, imageCenter.real+imageRowSize/2):
-        self.fractalImage.sappend([])
-        for col in range(imageCenter.imag-imageColSize/2, imageCenter.imag+imageRowSize/2):
-            fractalImage[row][col].append(fractalSet[row][col].iterationsToThreshhold())
-    
-    # Plots the generated image.
-    def show(self):
-    
-        matplotlib.pyplot.imshow(self.fractalImage, aspect=1.1, extent=(xvals.min(),xvals.max(),yvals.min(),yvals.max()))
-        matplotlib.pyplot.xlabel('Real Axis')
-        matplotlib.pyplot.ylabel('Imaginary Axis')
-        matplotlib.pyplot.title('Mandelbrot Fractal')
+from FractalSet import FractalSet
+from FractalImage import FractalImage
 
 def main():
     
     # Gets initial parameters for generation of the fractal image.
     
-    initialParameter = input('For what initial parameter should the set be calculated? (0 for Mandelbrot Set): ')
+    initialParameter = input('For what initial parameter should the fractal be generated? (0 for Mandelbrot Set): ')
+    print('Over what range of complex parameters should the fractal be generated?')
+    minimumComplexParameter = input('Minimum complex parameter (A + B * 1j): ')
+    maximumComplexParameter = input('Maximum complex parameter (A + B * 1j): ')
+    thresholdValue = input('What is the threshold value for divergence: ')
     imageRowSize = input('How many pixels wide should the generated image be: ')
     imageColSize = input('How many pixels tall should the generated image be: ')
     
@@ -77,14 +32,25 @@ def main():
     else:
         realCenter = float(input('Real center coordinate: '))
         imaginaryCenter = float(input('Imaginary center coordinate: '))
-        imageCenter = realCenter + imaginaryCenter * 0j
+        imageCenter = realCenter + imaginaryCenter * 1j
     
     # Gets more initial parameters for the generation of the fractal image.
-    
+      
     maxIterations = int(input('To how many iterations should the fractal be generated: '))
     
     print('Generating image...')
-    fractalImage = FractalImage(initialParameter, imageRowSize, imageColSize, imageCenter, maxIterations)
+    
+    # Calculates the step size as a complex number.
+
+    step = (maximumComplexParameter - minimumComplexParameter) / (imageRowSize + imageColSize * 1j)
+    
+    # Generates a fractal set from the given parameters.
+
+    fractalSet = FractalSet(minimumComplexParameter, maximumComplexParameter, step, initialParameter, thresholdValue, maxIterations)
+    
+    # Generates a fractal image object from the fractal set and displays it.
+
+    fractalImage = FractalImage(fractalSet, minimumComplexParameter, maximumComplexParameter)
     fractalImage.show()
     
 main()
