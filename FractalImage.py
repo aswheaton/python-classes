7f20a09ce68c8ecc2ec855223ab290a97fe4a68a
Alex Wheaton <34541330+aswheaton@users.noreply.github.com>
2018-02-15 13:09:21 +0000

    Updated FractalImage.py

--- FractalImage.py
+++ FractalImage.py
@@ -8,20 +8,24 @@
     Update: 14th February 2018 
 """
 
+import cmath
+
+import matplotlib.pyplot
+
 from FractalSet import FractalSet
 
 class FractalImage(object):
     
     # Initialises an array-like list of lists containing the iterations to the threshold value of a fractal set object.
     
-    def __init__(self, fractalSet, minumimComplexParameter, maximumComplexParameter):
+    def __init__(self, fractalSet, minimumComplexParameter, maximumComplexParameter):
     
         self.fractalImage = []
         
-        for row in range(0, len(fractalSet.getSet())):
-            self.fractalImage.sappend([])
-            for col in range(0, len(fractalSet.getSet()[row])):
-                fractalImage[row][col].append(fractalSet.getSet[row][col].getIterationsToThreshhold())
+        for row in range(0, len(fractalSet.getFractalSet())):
+            self.fractalImage.append([])
+            for col in range(0, len(fractalSet.getFractalSet()[row])):
+                self.fractalImage[row].append(fractalSet.getFractalSet()[row][col].getIterationsToThreshold())
         
         self.minimum = minimumComplexParameter
         self.maximum = maximumComplexParameter
@@ -30,8 +34,10 @@ class FractalImage(object):
     
     def show(self):
     
-        matplotlib.pyplot.imshow(self.fractalImage, aspect=1.1, extent=(minimum.real, maximum.real, minimum.imag, maximum.imag))
+        matplotlib.pyplot.imshow(self.fractalImage, cmap='magma', aspect='auto', extent=(self.minimum.real, self.maximum.real, self.minimum.imag, self.maximum.imag))
         matplotlib.pyplot.xlabel('Real Axis')
         matplotlib.pyplot.ylabel('Imaginary Axis')
         matplotlib.pyplot.title('Mandelbrot Fractal')
+        matplotlib.pyplot.show()
 
+        print(self.fractalImage)
