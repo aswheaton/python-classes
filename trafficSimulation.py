a332a43025abbad0f645e52ebaefed70283478e0
Alex Wheaton <34541330+aswheaton@users.noreply.github.com>
2018-03-13 14:39:05 +0000

    Cleaned up comments and smoothed velocity vs. density function.

--- trafficSimulation.py
+++ trafficSimulation.py
@@ -2,7 +2,7 @@
     Test code for a traffic simulation class object.
     Author: Alexander S. Wheaton
     Date: 06 March 2018
-    Updated: 06 March January 2018
+    Updated: 13 March January 2018
 """
 
 import matplotlib.pyplot
@@ -14,7 +14,6 @@ def main():
     if str(raw_input("Simulate more than one density? (y/n): "))[0] == "y":
         
         # Get a bunch of initial parameters.
-        
         roadLength = input("How long should each road be: ")
         
         print("Over what range of densities should the simulations be run?")
@@ -25,7 +24,6 @@ def main():
         iterations = input("To how many iterations should each simulation be run: ")
         
         # Begins the simulation.
-        
         listOfDensities = []
         listOfVelocities = []
         
@@ -48,26 +46,31 @@ def main():
             # matplotlib.pyplot.imshow(simulation.getSimulation(), interpolation="none")
             # matplotlib.pyplot.show()
         
-        matplotlib.pyplot.plot(listOfDensities, listOfVelocities)
-        matplotlib.pyplot.autoscale(enable=True, axis="both", tight=False)
+        # Plots the steady state velocity of traffic against the traffic density.
+        matplotlib.pyplot.plot(listOfDensities, listOfVelocities, 'b.')
+        matplotlib.pyplot.autoscale(enable=True, axis="both", tight=True)
         matplotlib.pyplot.xlabel("Density")
         matplotlib.pyplot.ylabel("Average Velocity, Steady-State")
         matplotlib.pyplot.title("Average Velocity vs. Traffic Density")
         matplotlib.pyplot.show()
         
     else:
-    
+        
+        # Gets a bunch of initial parameters.
         roadLength = input("How long should the road be: ")
         density = float(input("What should the density of traffic be (float 0 < d < 1): "))
         iterations = input("To how many iterations should the simulation be run: ")
         
+        # Begins the simulation.
         simulation = Simulation(roadLength, density)
         
         for i in range(iterations):
             simulation.iterate()
-            
-        simulation.show()
         
+        # Outputs the result of the simulation to the console.        
+        # simulation.show()
+        
+        # Displays a graphic illustrating the simulation.
         matplotlib.pyplot.imshow(simulation.getSimulation(), interpolation="none")
         matplotlib.pyplot.show()
 
