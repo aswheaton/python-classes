d3edb1682cf1c8a4c65c751b0418f999a29efa1a
Alex Wheaton <alex.s.wheaton@gmail.com>
2018-10-03 22:16:07 +0100

    Added MuonDecaySet class file.

---
+++ MuonDecaySet.py
@@ -0,0 +1,23 @@
+"""
+    Dataset of simulated Muon decay times using the Monte Carlo technique.
+    Author: Alexander S. Wheaton
+    Date: 3 October 2018
+    Updated: 3 October 2018
+"""
+
+class(object) MuonDecaySet(self):
+
+    def __init__(self, tau, size):
+        
+        self.dataset = []
+        
+        self.tau = tau
+        
+        self.size = size
+    
+    def generateSet(self):
+    
+    def writeToFile(self):
+    
+    def plotHistogram(self):
+    
