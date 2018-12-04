Fixed error calculation, again. Maybe.

Final Project/test.py
@@ -36,19 +36,21 @@ def PDF_t(time, **kwargs):
 
 def main():
 
-    #dataset1 = DecaySet(load=False)
-    #dataset1.generateSet(10, 2*np.pi, function=PDF, pdfMax=0.13, size=10000, tau1=1.0, tau2=2.0, frac=0.5)
-    #dataset1.plotHistogram(100, write=False)
+    dataset1 = DecaySet(load=False)
+    dataset1.generateSet(10, 2*np.pi, function=PDF, pdfMax=0.13, size=10000, tau1=1.0, tau2=2.0, frac=0.5)
+    dataset1.plotHistogram(100, write=False)
     
     dataset2 = DecaySet(load=True, filename='datafile-Xdecay.txt', column=1)
     paramFit = ParameterFit(dataset=dataset2.get(), function=PDF_t)
     tau1, tau2, frac = paramFit.maxLikelihood(tau1=1.0, tau2=2.0, frac=0.5, limit_tau1=(0.001,10), limit_tau2=(0.001,10), limit_frac=(0,1), errordef=0.5, pedantic=False)
-    print('The fitted parameters are tau1={}, tau2={}, frac={}'.format(tau1, tau2, frac))
+    tau1_err, tau2_err, frac_err = paramFit.simpleErrors(step=0.001)
+    print('The fitted parameters are tau1={}+/-{}, tau2={}+/-{}, frac={}+/-{}'.format(tau1, tau1_err, tau2, tau2_err, frac, frac_err))
 
     dataset3 = DecaySet(load=True, filename='datafile-Xdecay.txt')
     paramFit = ParameterFit(dataset=dataset3.get(), function=PDF)
-    tau1, tau2, frac = paramFit.maxLikelihood(tau1=1.0, tau2=2.0, frac=0.5, limit_tau1=(0.001,10), limit_tau2=(0.001,10), limit_frac=(0,1), errordef=0.5, pedantic=False)
-    print('The fitted parameters are tau1={}, tau2={}, frac={}'.format(tau1, tau2, frac))
+    tau1, tau2, frac = paramFit.maxLikelihood(tau1=1.0, tau2=2.0, frac=0.5, limit_tau1=(0.01,10), limit_tau2=(0.001,10), limit_frac=(0,1), errordef=0.5, pedantic=False)
+    tau1_err, tau2_err, frac_err = paramFit.simpleErrors(step=0.001)
+    print('The fitted parameters are tau1={}+/-{}, tau2={}+/-{}, frac={}+/-{}'.format(tau1, tau1_err, tau2, tau2_err, frac, frac_err))
 
 main()
 