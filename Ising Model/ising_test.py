10fdede551728077ca534aa789509dd111cd796c
Alex Wheaton <alex.s.wheaton@gmail.com>
2020-01-20 00:04:18 +0000

    Renamed test script and reduced max_iter pass.

---
+++ Ising Model/ising_test.py
@@ -0,0 +1,5 @@
+from Ising_Lattice import Ising_Lattice
+import matplotlib.pyplot as plt
+
+lattice = Ising_Lattice(temperature=1.5, size=(100,100), mode="r", animate=True)
+lattice.run(max_iter=1000)
--- Ising Model/test.py
+++
@@ -1,5 +0,0 @@
-from Ising_Lattice import Ising_Lattice
-import matplotlib.pyplot as plt
-
-lattice = Ising_Lattice(temperature=1.5, size=(100,100), mode="r", animate=False)
-lattice.run(max_iter=100000)
