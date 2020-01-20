3d60164c5d433b805c156a67f2c7f6bebdeb91d2
Alex Wheaton <alex.s.wheaton@gmail.com>
2020-01-20 15:50:26 +0000

    Rearranged test code.

--- Ising Model/dynamic_image.py
+++ Ising Model/dynamic_image.py
@@ -9,8 +9,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-fig = plt.figure()
-
+def updatefig(*args):
+    global x, y
+    x += np.pi / 15.
+    y += np.pi / 20.
+    im.set_array(f(x, y))
+    return im,
 
 def f(x, y):
     return np.sin(x) + np.cos(y)
@@ -18,15 +22,8 @@ def f(x, y):
 x = np.linspace(0, 2 * np.pi, 120)
 y = np.linspace(0, 2 * np.pi, 100).reshape(-1, 1)
 
+fig = plt.figure()
 im = plt.imshow(f(x, y), animated=True)
-
-
-def updatefig(*args):
-    global x, y
-    x += np.pi / 15.
-    y += np.pi / 20.
-    im.set_array(f(x, y))
-    return im,
 
 ani = animation.FuncAnimation(fig, updatefig, interval=50, blit=True)
 plt.show()
