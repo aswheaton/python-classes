4a2209ccf413ce2b48743349aaca8b7e43c07564
Alex Wheaton <alex.s.wheaton@gmail.com>
2020-01-24 16:02:38 +0000

    Implemented Kawasaki dynamics.

--- Ising Model/ising_test.py
+++ Ising Model/ising_test.py
@@ -2,4 +2,4 @@ from Ising_Lattice import Ising_Lattice
 import matplotlib.pyplot as plt
 
 lattice = Ising_Lattice(temperature=1.5, size=(100,100), mode="r", animate=True)
-lattice.run(dynamic="glauber", max_iter=1000)
+lattice.run(dynamic="kawasaki", max_iter=10000)
